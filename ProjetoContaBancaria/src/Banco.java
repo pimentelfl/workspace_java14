211e4a7d68fa2562fa65e89e0bc08ee340119f71
pimentelfl <pimentel.fl@gmail.com>
2021-02-03 13:12:45 -0300

    getters setters e private

---
+++ ProjetoContaBancaria/src/Banco.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Banco {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		ContaBancaria c1 = new ContaBancaria();
+		
+		int opcao;
+		double valor;
+	
+		c1.modificarNumeroConta(12345);
+		c1.digitoVerif = 6;
+		c1.cpfTitular = "123.456.789-00";
+		c1.nomeTitular = "Fabio LP";
+		c1.saldo = 0.0;
+		
+		do {
+			System.out.println("Banco FPItau - Feito para voce");
+			System.out.println("Digite 1 - Deposito / 2 - Saque / 3 - Infor / 0 - Encerrar");
+			opcao = teclado.nextInt();
+			switch(opcao) {
+			case 1:
+				System.out.print("Digite o valor do Deposito:");
+				valor = teclado.nextDouble();
+				c1.creditar(valor);
+				break;
+			case 2:
+				System.out.println("Digite o valor do Saque:");
+				valor = teclado.nextDouble();
+				
+				if (c1.debitar(valor)) {
+					System.out.println("Saque autorizado!");
+				}
+				else {
+					System.out.println("Saque não autorizado");
+				}
+				break;
+			case 3:
+					System.out.println(c1.exibirInfo());
+					break;
+			case 4:
+					System.out.println("Obrigado pela preferencia");
+					break;
+			default:
+					System.out.println("Opcao invalida! Tente novamente");
+			}
+		
+		} while (opcao != 0);
+	}	
+}
--- ProjetoContaBancaria/src/ContaBancaria.java
+++ ProjetoContaBancaria/src/ContaBancaria.java
@@ -1,22 +1,40 @@
 
 public class ContaBancaria {
-	int numConta;
-	String nomeTitular;
-	double saldo;
+	private int 	numeroConta;
+	private int 	digitoVerif;
+	private String 	cpfTitular;
+	private String 	nomeTitular;
+	private double 	saldo;
+	
+	void modificarNumeroConta(int novoNumero) {
+		if (novoNumero <= 0) {
+			// Erro
+		}
+		numeroConta = novoNumero;
+	}
+	
+	int consultarNumeroConta( ) {
+		return numeroConta;
+	}
+	
+	String exibirInfo() {
+		return numeroConta+"-"+digitoVerif+" : "+nomeTitular+" ("+cpfTitular+") "+saldo;
+		
+	}
 	
 	void creditar(double valor) {
 		saldo += valor;
 	}
-	
 	boolean debitar(double valor) {
-		if(saldo >= valor) {
+		if(valor <= saldo) {
 			saldo -= valor;
 			return true;
 		}
-		return false;
+		else {
+			return false;
+		}
 	}
 	
-	String exibirInfo() {
-		return numConta + " - " + nomeTitular + " R$" +saldo;
-	}
+	
+
 }
